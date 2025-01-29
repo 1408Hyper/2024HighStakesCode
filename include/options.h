2b31d41a8ba392bf45fab10189f8d5284cacdce7
helloworld3200 <helloworld32.tech@gmail.com>
2025-01-29 13:12:14 +0000

    update options.h and main.cpp

--- include/options.h
+++ include/options.h
@@ -47,7 +47,7 @@
 #define IMU_PORT 12
 
 // Turn on/off auton and opcontrol
-#define DO_MATCH_AUTON true
+#define DO_MATCH_AUTON false
 #define DO_SKILLS_AUTON false
 
 // Turn on for match auton to be run at the start of opcontrol
